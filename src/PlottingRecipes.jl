ec0ca2cdb982c06a36ca0efac082af6c2f377c22
Oliver Schulz <oschulz@mpp.mpg.de>
2018-10-29 16:57:46 +0100

    Initial project layout

---
+++ src/PlottingRecipes.jl
@@ -0,0 +1,9 @@
+# This file is a part of PlottingRecipes.jl, licensed under the MIT License (MIT).
+
+__precompile__(true)
+
+module PlottingRecipes
+
+# package code goes here
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,8 @@
+# This file is a part of PlottingRecipes.jl, licensed under the MIT License (MIT).
+
+import Test
+Test.@testset "Package PlottingRecipes" begin
+
+# test code goes here
+
+end # testset
