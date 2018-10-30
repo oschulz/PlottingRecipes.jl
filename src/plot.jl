3cfd02a41d8619e23fe3bf12ceda82e070ed04e9
Oliver Schulz <oschulz@mpp.mpg.de>
2018-10-30 17:38:17 +0100

    STASH

--- src/plot.jl
+++ src/plot.jl
@@ -7,9 +7,31 @@ function plot! end
 export plot!
 
 
-abstract type AbstractFigure end
+abstract type AbstractCanvas end
+
+abstract type AbstractFigure <: AbstractCanvas end
 export AbstractFigure
+
+
+abstract type AbstractLegendSymbol <: AbstractCanvas end
+export AbstractLegendSymbol
+
 
 
 function plotting_recipe! end
 export plotting_recipe!
+
+
+function plot_impl end
+function plot_impl! end
+
+
+# global variables:
+current_figure = nothing
+current_engine = nothing
+
+
+plot(args...) = plot_impl(current_engine, args...)
+
+plot!(figure::AbstractFigure, args...) = plot_impl!(current_engine, figure, args...)
+plot!(args...) = plot_impl!(current_engine, current_figure, args...)
